1c8e62be54cd33c815866fbc64627360ea14746e
Salah Alaoui <salahalaoui@Salahs-Mac-Perso.local>
2025-11-30 21:40:10 +0100

    wip

--- terraform/vault_server/vault.tf
+++ terraform/vault_server/vault.tf
@@ -77,13 +77,3 @@ resource "helm_release" "vault" {
   cleanup_on_fail = true
 }
 
-# Output the Vault service details
-output "vault_service_name" {
-  value       = "${helm_release.vault.name}-ui"
-  description = "Vault UI service name"
-}
-
-output "vault_namespace" {
-  value       = kubernetes_namespace.vault.metadata[0].name
-  description = "Vault namespace"
-}
