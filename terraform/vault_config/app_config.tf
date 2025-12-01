d546f595462bb9dece8be14e323a8ba52a3223f7
Salah Alaoui <salahalaoui@Salahs-Mac-Perso.local>
2025-12-01 18:32:45 +0100

    wip

--- terraform/vault_config/app_config.tf
+++ terraform/vault_config/app_config.tf
@@ -20,6 +20,25 @@ resource "vault_kubernetes_auth_backend_role" "this" {
   bound_service_account_names      = [data.kubernetes_service_account.this.metadata[0].name]
   bound_service_account_namespaces = [resource.kubernetes_namespace.app.metadata[0].name]
   token_ttl                        = 3600  # 1 hour
-  token_policies                   = ["default"]
+  token_policies                   = ["default", resource.vault_policy.webapp_policy.name]
   audience                         = "https://kubernetes.default.svc.cluster.local"  # Match the default K8s audience
+}
+
+
+
+resource "vault_policy" "webapp_policy" {
+  name = "webapp-policy"
+
+  policy = jsonencode({
+    path = {
+      # Grants read access to the data in the KVv2 secret path
+      "kvv2/data/webapp/*" = {
+        capabilities = ["read"]
+      }
+      # Grants list/read access to the metadata (required for lease management/rotation)
+      "kvv2/metadata/webapp/*" = {
+        capabilities = ["read", "list"]
+      }
+    }
+  })
 }
